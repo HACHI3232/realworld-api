fe34c8ea2692f620d8e24257da884aab2f712f84
HACHI3232 <harada.chisato526@gmail.com>
2024-01-28 15:08:03 +0900

    ルーティングとコントローラー追加

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,37 @@
+class ArticlesController < ApplicationController
+  def create
+    article = Article.new(article_params)
+    if article.save
+      render json: article, status: :created
+    else
+      render json: article.errors, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    article = Article.find(params[:id])
+    render json: article
+  end
+  
+  def update
+    article = Article.find(params[:id])
+    if article.update(article_params)
+      render json: article
+    else
+      render json: article.errors, status: :unprocessable_entity
+    end
+  end
+  
+  def destroy
+    article = Article.find(params[:id])
+    article.destroy
+    head :no_content
+  end
+  
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+end
