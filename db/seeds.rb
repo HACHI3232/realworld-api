3b13c224419ca7f854f7c7db4965199dc3beff34
HACHI3232 <harada.chisato526@gmail.com>
2024-01-28 17:05:38 +0900

    SEED値を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,6 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+5.times do |i|
+  Article.create!(
+    title: "テスト記事 #{i + 1}",
+    body: "これはテスト記事 #{i + 1} の本文です。これはサンプルテキストです。"
+  )
+end
