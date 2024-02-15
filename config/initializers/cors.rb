e335f8ef7ecc93fffab56d3bac0f7f6eef019acd
HACHI3232 <harada.chisato526@gmail.com>
2024-02-16 00:18:21 +0900

    URLのミスを修正

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3001","https;//aws-hachi.net"
+    origins "http://localhost:3001","https://web.aws-hachi.net"
 
     resource "*",
       headers: :any,
