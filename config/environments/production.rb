7a0302b46ed21133494914db81c5085677daa4bd
HACHI3232 <harada.chisato526@gmail.com>
2024-02-18 19:14:03 +0900

    wwwを追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,5 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
   config.hosts << "aws-hachi.net"
+  config.hosts << "www.aws-hachi.net"
 end
